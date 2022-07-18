ae21f6ce5777faa333484e43080a47eb206ed27e
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-07-18 16:03:24 +0200

    Add BookShelf component

---
+++ src/components/BookShelf.js
@@ -0,0 +1,28 @@
+import Book from './Book';
+
+const BookShelf = ({ title, books, shelfName, onChangeShelf }) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books
+            .filter((b) => b['shelf'] === shelfName)
+            .map((b) => {
+              return (
+                <li key={b['id']}>
+                  <Book
+                    book={b}
+                    onChangeShelf={onChangeShelf}
+                    existing={true}
+                  />
+                </li>
+              );
+            })}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default BookShelf;
