0101a9d02cfd7db58360ec4df91ef42a09a038b0
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-07-18 15:55:18 +0200

    Add Search Page component

---
+++ src/pages/Search.js
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Book from '../components/Book';
+
+const Search = ({
+  booksSearched,
+  existingBooks,
+  onHandleSearch,
+  onChangeShelf,
+}) => {
+  const [Value, setValue] = useState('');
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+    onHandleSearch(e.target.value.trim());
+  };
+
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <Link className="close-search" to="/" reloadDocument={true}>
+          Close
+        </Link>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search by title, author, or ISBN"
+            value={Value}
+            onChange={onChange}
+          />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid">
+          {booksSearched.map((book) => {
+            return (
+              <li key={book['id']}>
+                {existingBooks.filter(
+                  (b) =>
+                    b['title'] === book['title'] &&
+                    b['authors'][0] === book['authors'][0]
+                ).length === 1 ? (
+                  <Book
+                    book={
+                      existingBooks.filter(
+                        (b) => b['title'] === book['title']
+                      )[0]
+                    }
+                    onChangeShelf={onChangeShelf}
+                    existing={true}
+                  />
+                ) : (
+                  <Book
+                    book={book}
+                    onChangeShelf={onChangeShelf}
+                    existing={false}
+                  />
+                )}
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
