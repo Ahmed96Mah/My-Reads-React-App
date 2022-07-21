bc71663b46fd5bf4c579bc2a77d054f92c89e492
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-07-21 20:26:13 +0200

    Update BooksAPI file

--- src/util/BooksAPI.js
+++ src/util/BooksAPI.js
@@ -4,8 +4,12 @@ let token = localStorage.token;
 
 const searchforUser = () => {
   const storageKeys = Object.keys(localStorage);
-  const test = storageKeys.filter((key) => key.match('user')).filter((key) => localStorage[key].split(',')[3] === localStorage["token"]);
-  (test.length === 0)? (localStorage.loggedIn = false):(localStorage.loggedIn = true);
+  const test = storageKeys
+    .filter((key) => key.match('user'))
+    .filter((key) => localStorage[key].split(',')[3] === localStorage['token']);
+  test.length === 0
+    ? (localStorage.loggedIn = false)
+    : (localStorage.loggedIn = true);
 };
 
 searchforUser();
@@ -51,22 +55,43 @@ export const search = (query, maxResults) =>
     .then((data) => data.books);
 
 export const createUser = (user) => {
+  // Generate a random token for the user
   const userToken = Math.random().toString(36).substring(-8);
-  if (user.username !== "" && user.email !== "" && user.password !== "") {
+  // make sure that the data sent by the form includes all the required fields
+  if (user.username !== '' && user.email !== '' && user.password !== '') {
     const storageKeys = Object.keys(localStorage);
-    const test = storageKeys.filter((key) => key.match('user')).filter((key) => localStorage[key].split(',')[0] === user.username || localStorage[key].split(',')[1] === user.email);
-    if(test.length === 0) {
-      localStorage[`user${localStorage.length}`] = `${user.username},${user.email},${user.password},${userToken}`;
+    // check if the username or e-mail is already used by another user
+    const test = storageKeys
+      .filter((key) => key.match('user'))
+      .filter(
+        (key) =>
+          localStorage[key].split(',')[0] === user.username ||
+          localStorage[key].split(',')[1] === user.email
+      );
+    if (test.length === 0) {
+      // if no one uses that user name or e-mail, save them along with the password and auth token
+      localStorage[
+        `user${localStorage.length}`
+      ] = `${user.username},${user.email},${user.password},${userToken}`;
       localStorage.token = userToken;
-      return true;
-    }else {
-      return false;
+      return true; // indicate a successfull post operation
+    } else {
+      return false; // indicate a failed post operation
     }
   }
 };
 
 export const checkUser = (user) => {
   const storageKeys = Object.keys(localStorage);
-  const test = storageKeys.filter((key) => key.match('user')).filter((key) => localStorage[key].split(',')[0] === user.username && localStorage[key].split(',')[2] === user.password);
-  (test.length === 1) && (localStorage.token = localStorage.getItem(test[0]).split(',')[3]);
+  // check that the username and password combo existis
+  const test = storageKeys
+    .filter((key) => key.match('user'))
+    .filter(
+      (key) =>
+        localStorage[key].split(',')[0] === user.username &&
+        localStorage[key].split(',')[2] === user.password
+    );
+  // if so, set the currently used token in localstorage to the user's token
+  test.length === 1 &&
+    (localStorage.token = localStorage.getItem(test[0]).split(',')[3]);
 };
